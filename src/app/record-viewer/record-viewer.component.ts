447f307bdadd1a9c73912c9577ea4d0567b2c3c2
Orion Wolf-Hubbard <orion@Orions-MacBook-Pro.local>
2019-08-08 08:38:05 -0400

    added highscores

--- src/app/record-viewer/record-viewer.component.ts
+++ src/app/record-viewer/record-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-record-viewer',
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordViewerComponent implements OnInit {
   games: { gameNumber: number, playerOne: string, playerTwo: string, scoreOne: number, scoreTwo: number}[] = [];
+  @ViewChild('player') player: ElementRef;
+  @ViewChild('playerOne') playerOne: ElementRef;
+  @ViewChild('playerTwo') playerTwo: ElementRef;
+  names;
 
   constructor() { }
 
   ngOnInit() {
+    this.getNames();
+    setTimeout(() => this.getNames(), 50);
   }
 
   viewAll() {
@@ -22,5 +28,34 @@ export class RecordViewerComponent implements OnInit {
 
   clear() {
     this.games = [];
+  }
+
+  getNames() {
+    fetch('https://catalyte-pong.herokuapp.com/players/list', {mode: 'cors'}).then(res => res.json())
+      .then(result => {
+        this.names = result;
+        this.player.nativeElement.value = null;
+        this.playerOne.nativeElement.value = null;
+        this.playerTwo.nativeElement.value = null;
+      });
+  }
+
+  lookupPlayer(player) {
+    if (player) {
+      fetch('https://catalyte-pong.herokuapp.com/games/player?player=' + player, {mode: 'cors'}).then(res => res.json())
+        .then(result => {
+          this.games = result;
+        });
+    }
+  }
+
+  lookupPlayers(playerOne, playerTwo) {
+    if (playerOne || playerTwo) {
+      fetch(`https://catalyte-pong.herokuapp.com/games/vs?playerOne=${playerOne}&playerTwo=${playerTwo}`, {mode: 'cors'})
+        .then(res => res.json())
+        .then(result => {
+          this.games = result;
+        });
+    }
   }
 }
