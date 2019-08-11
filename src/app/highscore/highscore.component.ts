22b453d5c69630253b6a3062cb537d5895e3ca1d
Orion Wolf-Hubbard <orion@Orions-MBP.fios-router.home>
2019-08-11 08:17:53 -0400

    winPercent

--- src/app/highscore/highscore.component.ts
+++ src/app/highscore/highscore.component.ts
@@ -16,15 +16,30 @@ export class HighscoreComponent implements OnInit {
   }
 
   fetchScores(type) {
-    this.players = [];
     fetch('https://catalyte-pong.herokuapp.com/players/scores?type=' + type, {mode: 'cors'}).then(res => res.json())
       .then(result => {
         this.players = result;
       });
   }
 
-  format(rating) {
-    rating = rating + '';
-    return rating.split('.')[0];
+  round(number) {
+    const roundUp = ['5', '6', '7', '8', '9'];
+    number = number + '';
+    const split = number.split('.');
+    let result = split[0] * 1;
+    if (split.length > 1) {
+      const firstDecimal = split[1].substring(0, 1);
+      if (roundUp.includes(firstDecimal)) {
+        result++;
+      }
+    }
+    return result;
+  }
+
+  winPercent(player) {
+    const wins = player.wins;
+    const total = player.wins + player.losses;
+    const result = wins / total * 100;
+    return this.round(result) + '%';
   }
 }
